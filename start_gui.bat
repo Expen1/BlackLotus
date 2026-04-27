d2126ac3206172c134ecaf56606e3a8ab86c368e
Sa <97338349+Expen1@users.noreply.github.com>
2026-04-27 12:29:46 +0330

    Create start_gui.bat

---
+++ start_gui.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 >nul
+title BlackLotus Ver - Domain Fronting
+echo.
+echo ================================================
+echo        ⚡ BLACK LOTUS VER - Professional GUI
+echo ================================================
+echo.
+
+python gui.py
+
+echo.
+echo برنامه بسته شد
+pause
